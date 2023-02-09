d5e924ed5162cbd6b15b16f2d0b7d498298ffb71
FollowerOfTheOmnisiah <wmb7648@gmail.com>
2023-02-08 19:12:06 -0500

    Gyro implementing IGyro

--- src/main/java/com/team871/config/Gyro.java
+++ src/main/java/com/team871/config/Gyro.java
@@ -1,13 +1,12 @@
 package com.team871.config;
 
 import com.kauailabs.navx.frc.AHRS;
-import edu.wpi.first.util.sendable.Sendable;
 import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.Commands;
 
-public class Gyro implements Sendable {
+public class Gyro implements IGyro {
   private final AHRS gyro = new AHRS();
 
   @Override
