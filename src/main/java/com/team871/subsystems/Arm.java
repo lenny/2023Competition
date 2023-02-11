122505ca8967a241660926087eccf261e51b5b91
FollowerOfTheOmnisiah <wmb7648@gmail.com>
2023-02-11 13:42:05 -0500

    arm subsystem

--- src/main/java/com/team871/subsystems/Arm.java
+++ src/main/java/com/team871/subsystems/Arm.java
@@ -1,3 +1,31 @@
 package com.team871.subsystems;
 
-public class Arm {}
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Arm extends SubsystemBase {
+  private final MotorController shoulderMotor;
+  private final MotorController extensionMotor;
+
+  public Arm(final MotorController shoulder, final MotorController extetensionMotor) {
+    this.shoulderMotor = shoulder;
+    this.extensionMotor = extetensionMotor;
+  }
+
+  public void moveShoulderPitch(final double output) {
+    shoulderMotor.set(output);
+  }
+
+  public void moveExtension(final double output) {
+    extensionMotor.set(output);
+  }
+
+  public CommandBase moveShoulderPitchCommand(final double output) {
+    return run(() -> moveShoulderPitch(output));
+  }
+
+  public CommandBase moveExtensionCommand(double output) {
+    return run(() -> moveExtension(output));
+  }
+}
