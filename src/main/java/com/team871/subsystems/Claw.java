47f7cffa0b7a2463d3c26feb8f60f690e805864c
FollowerOfTheOmnisiah <wmb7648@gmail.com>
2023-02-11 13:42:22 -0500

    claw subsystem

--- src/main/java/com/team871/subsystems/Claw.java
+++ src/main/java/com/team871/subsystems/Claw.java
@@ -1,3 +1,34 @@
 package com.team871.subsystems;
 
-public class Claw {}
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Claw extends SubsystemBase {
+  private static double INTAKE_MOTOR_SPEED = .02;
+
+  private final MotorController pinchMotor;
+  private final MotorController intakeMotors;
+
+  public Claw(
+      final MotorController pinchMotor,
+      final MotorController leftIntakMotor,
+      final MotorController rightIntakeMotor) {
+    this.pinchMotor = pinchMotor;
+
+    intakeMotors = new MotorControllerGroup(leftIntakMotor, rightIntakeMotor);
+  }
+
+  public void setPinch(final double output) {
+    pinchMotor.set(output);
+  }
+
+  public void toggleIntakeMotorsCommand(final boolean b) {
+    intakeMotors.set(b ? INTAKE_MOTOR_SPEED : 0);
+  }
+
+  public CommandBase toggleIntakeMotors(final boolean b) {
+    return run(() -> toggleIntakeMotors(b));
+  }
+}
