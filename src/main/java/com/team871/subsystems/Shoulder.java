562fd742abdf160d30f459f1e70ec5a88f902bbe
Lenny Marks <lenny@lennymarks.com>
2023-02-19 12:42:42 -0500

    More descriptive naming in Shoulder

--- src/main/java/com/team871/subsystems/Shoulder.java
+++ src/main/java/com/team871/subsystems/Shoulder.java
@@ -3,29 +3,28 @@ package com.team871.subsystems;
 import com.team871.config.PitchEncoder;
 import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import java.util.function.Supplier;
 
 public class Shoulder extends SubsystemBase {
   private final MotorController shoulderMotor;
-  private PitchEncoder shoulderEncoder;
+  private final PitchEncoder pitchEncoder;
 
-  public Shoulder(final MotorController shoulder, final PitchEncoder shoulderEncoder) {
+  public Shoulder(final MotorController shoulder, final PitchEncoder pitchEncoder) {
     this.shoulderMotor = shoulder;
-    this.shoulderEncoder = shoulderEncoder;
+    this.pitchEncoder = pitchEncoder;
   }
 
   public void moveShoulderPitch(final double output) {
+    SmartDashboard.putNumber("pitchMotor", output);
     shoulderMotor.set(output);
-    SmartDashboard.putNumber("shoulderEncoder", shoulderEncoder.getPitch());
   }
 
-  public CommandBase moveShoulderPitchCommand(final double output) {
-    return run(() -> moveShoulderPitch(output));
-  }
-
-  public void setdefaultCommand(Supplier<Double> shoulderPitch) {
+  public void setDefaultCommand(Supplier<Double> shoulderPitch) {
     setDefaultCommand(run(() -> moveShoulderPitch(shoulderPitch.get())));
+  }
+
+  public double getPitch() {
+    return pitchEncoder.getPitch();
   }
 }
